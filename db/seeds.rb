fc1b2fe5696524599f66ad8892737d5ee17af488
Wade Tandy <wtandy@bloomberg.net>
2017-05-23 15:05:37 -0400

    Add faker to seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
 [Employee, Position, Department].each(&:delete_all)
 
 @sales = Department.create!(name: 'Sales')
